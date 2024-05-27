544b4586f1c6ca4a2cc2c012f302674a72402393
Артемий <artema121rus@mail.ru>
2024-05-27 10:59:29 +0300

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://utfs.io/f/0e658cc6-fa7d-4450-9466-0f453b5d8df8-waepav.jpg",
   "https://utfs.io/f/608ee1dc-3f96-4c63-b8e7-494336f4531a-gct86v.jpg",
